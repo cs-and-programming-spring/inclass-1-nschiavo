ca1990c1adea9c59be066d75e576d1af83a009fd
Nicholas Schiavo <schiav55@rowan.edu>
2019-01-22 19:34:05 -0500

    Changed the Name and Email

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,8 @@
 
 
 /***********************************************
-* Name: Takeshwari Kamal
-* Email: kamalt4@students.rowan.edu
+* Name: Schiavo Nicholas
+* Email: schiav55@students.rowan.edu
 ***********************************************
 * Purpose:
 *  This program calculates area of circle
