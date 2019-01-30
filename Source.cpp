a8a7c54031a6e769640a0b8da7a793ac28ff0622
Nicholas Schiavo <schiav55@rowan.edu>
2019-01-29 19:56:45 -0500

    Changes

--- Source.cpp
+++ Source.cpp
@@ -6,37 +6,50 @@
 * Email: schiav55@students.rowan.edu
 ***********************************************
 * Purpose:
-*  This program calculates area of circle
+*  This program converts a distance in miles to kilometers, meters, centimeters, and millimeters. Then the program calculates the circuference of a circle given its radius.
 ***********************************************/
 
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
 const  float PI = 3.14159;
 
+
 int main()
 {
+	double miles, kilometers, meters, centimeters, millimeters;
+	cout << "********************** \n";
+	cout << "This program will convert your distance in Miles to Kilometers \n";
+	cout << "********************** \n\n";
+	cout << "Input the number of miles. \n";
+	//user inputs miles
+	cin >> miles;
+	//this converts the km to miles
+	kilometers = miles * 1.6;
+	cout << kilometers << " kilometers.\n\n";
+	// this converts km to meters, then centimeters, then millimeters
+	meters = kilometers * 1000;
+	cout << "That is " << meters << " meters. \n\n";
+	centimeters = meters * 100;
+	cout << "Or " << centimeters << " centimeters. \n\n";
+	millimeters = meters * 1000;
+	cout << "Or " << millimeters << " millimeters. \n\n";
 
-	float radius, area, circumference;
-	
-	//Display purpose of the program
-	cout << " ******************************************"<<endl;
-	cout << " This program takes in radius to compute area of circle" <<endl;
-	cout << " ******************************************\n\n";
-	
-	//Get user input
-	cout << "Please input radius: ";
+
+	double radius, circumference;
+	cout << "********************** \n";
+	cout << "This program will calculate the circumference of a circle given its radius. \n";
+	cout << "********************** \n\n";
+	cout << "Enter the radius of the circle. \n\n";
+	//user inputs radius
 	cin >> radius;
-	
-	//Compute area to PI*radius*radius
-	area = PI* radius *radius; 
-	
-	//Display the area 
-	cout << " The area is = " << area << "\n\n";
-	cout << "Program has ended"<<endl;
+	//calculate circumference
+	circumference = 2 * PI * radius;
+	cout << circumference << " is the circumference of the circle. \n\n";
+	cout << "The program has ended. \n";
 
 	system("pause");
-	
+
 	return 0;
 }
